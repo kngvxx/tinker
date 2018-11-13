e88c62db8655c88c393b7cc796c1d93d9dc2924e
kngvxx <kiki.puk@op.pl>
2018-11-13 17:45:05 +0100

    Clear main file

--- tinker/main.py
+++ tinker/main.py
@@ -1,12 +1,4 @@
 from tkinter import *
 window=Tk()
-label=Label(window, text="napis")
-def hello():
-    label['text']='inny'
 
-    btn=Button(window,text="guzik",command=hello)
-
-    btn.pack()
-    label.pack()
-
-    window.mainloop()
+window.mainloop()
