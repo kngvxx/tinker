3ed7652ba856e45fa2614d16e1554bf22ab70783
kngvxx <kiki.puk@op.pl>
2018-12-11 17:30:01 +0100

    HALO

--- tinker/main.py
+++ tinker/main.py
@@ -1,12 +1,20 @@
 from task import *
+from scrollable import *
+
 window = Tk()
+window.geometry("400x400+0+0")
+window.resizable(False, False)
+tasksFrame = Frame(window)
+tasksFrame.pack()
+scrollable_body = Scrollable(tasksFrame, width=20)
 
 entry = Entry(window, text="")
 entry.pack(side="bottom", fill="x")
 
-addTaskBtn = Button(window, text="dodaj zadanie", command=lambda:PrepareTask(window, entry))
+addTaskBtn = Button(window, text="dodaj zadanie", command=lambda:PrepareTask(scrollable_body, entry))
 addTaskBtn.pack(side="bottom", fill="x")
 
+# scrollbar.config(command=)
 # AddRowForTask(window, "1", "buton")
 # AddRowForTask(window, "2", "inna nazwa")
 window.mainloop()
--- tinker/task.py
+++ tinker/task.py
@@ -1,23 +1,36 @@
 from tkinter import *
 lineCount = 1
 
+archivedTasks = []
+
 def AddRowForTask(window, text, name):
-    frame = Frame(window)
+    frame = Frame(window, name=name)
     frame.pack()
-    checkbox = Checkbutton(frame, name=name+"cbx")
+    checkbox = Checkbutton(frame, name=name+"cbx", command=lambda: FinishTask(window, frame, text))
     checkbox.pack(side="left")
-    label = Label(frame, text=text)
+    label = Label(frame, text=text, height=2)
+    label.config(font=("Times New Roman", 12))
     label.pack(side="left")
-    editbtn = Button(frame, text="edytuj", name=name+"editbtn", command=lambda:Run(name+"editbtn"))
+    editbtn = Button(frame, text="edytuj", name=name+"editbtn", command=lambda: Run(name+"editbtn"))
     editbtn.pack(side="left")
-    deletebtn = Button(frame, text="usuń", name=name+"deletebtn", command=lambda:Run(name+"detelebtn"))
+    deletebtn = Button(frame, text="usuń", name=name+"deletebtn", command=lambda: Run(name+"detelebtn"))
     deletebtn.pack(side="left")
+
+
+def FinishTask(window, frame, text):
+    global archivedTasks
+    archivedTasks.append(text)
+    print(archivedTasks)
+    frame.pack_forget()
+    frame.destroy()
+    window.update()
 
 
 def PrepareTask(window, entry):
     global lineCount
     AddRowForTask(window, entry.get(), str(lineCount))
     lineCount += 1
+    window.update()
 
 
 def Run(name):
